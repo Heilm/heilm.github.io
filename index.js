39f8543e4e25e3b947cf48e3368ca96867fd94ed
Heilm <helmi.junaidy.lie@gmail.com>
2017-12-07 22:25:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 dragElement(document.getElementByID("roundbutton-container"));
 
-function dragElement(elmnt
+function dragElement(elmnt) 
+{
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementByID(elmnt.id + "header")) 
+  {document.getElementByID(elmnt.id + "header").onmousedown = dragMouseDown;}
+  else 
+  {elmnt.onmousedown = dragMouseDown;}
+  
+  function dragMouseDown(e)
+  {
+    e = e || window.event;
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+  
+  function closeDragElement()
+  {
+    document.onmouseup = null;
+    document.onmousemove=null;
+  }
+}
